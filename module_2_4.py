3a366bb753a78a29f9b57cb8cda734df39ad7f08
petruwka228 <artem.galitskiy.07@mail.ru>
2024-10-05 13:11:30 +0300

    Create module_2_4.py

---
+++ module_2_4.py
@@ -0,0 +1,17 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+is_number = False
+for i in range(len(numbers)):
+    if numbers[i] == 1:
+        continue
+    for j in range(2, numbers[i]):
+        if numbers[i]%j == 0:
+            primes.append(numbers[i])
+            is_number = True
+            break
+    if is_number == False:
+        not_primes.append(numbers[i])
+    is_number = False
+print(primes)
+print(not_primes)
