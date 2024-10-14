5faaf70c4713fbe79962513593a3cfb89bb50867
petruwka228 <artem.galitskiy.07@mail.ru>
2024-10-14 16:52:11 +0300

    Create module_3_4.py

---
+++ module_3_4.py
@@ -0,0 +1,6 @@
+def single_root_words(root_word, *other_words):
+    same_words = []
+    for i in other_words:
+        if root_word.lower() in i.lower() or i.lower() in root_word.lower():
+            same_words.append(i)
+    return same_words
