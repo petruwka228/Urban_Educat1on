244d77d8a1985d65cb84d63538baf527c4fef7c0
petruwka228 <artem.galitskiy.07@mail.ru>
2024-11-03 10:54:49 +0300

    aguagu

--- module_4_2.py
+++ module_4_2.py
@@ -2,4 +2,7 @@ def test_function():
     def inner_function():
         print("Я в области видимости функции test_function")
     inner_function()
-inner_function()
+try:
+    inner_function()
+except NameError as e:
+    print(e) # NameError: name 'inner_function' is not defined
