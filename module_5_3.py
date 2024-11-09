e1b0ddb2aee491d2ee89ec5065b9997fec7511db
petruwka228 <artem.galitskiy.07@mail.ru>
2024-11-09 14:33:50 +0300

    Update module_5_3.py

--- module_5_3.py
+++ module_5_3.py
@@ -2,39 +2,51 @@ class House:
     def __init__(self, name, number_of_floors):
         self.number_of_floors = number_of_floors
         self.name = name
+
     def __len__(self):
         return self.number_of_floors
+
     def __str__(self):
         return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
     def __eq__(self, other):
         if isinstance(other, House):
             return self.number_of_floors == other.number_of_floors
         else:
             return False
+
     def  __lt__(self, other):
         if isinstance(other, House):
             return self.number_of_floors < other.number_of_floors
+
     def __le__(self, other):
         if isinstance(other, House):
             return self.number_of_floors <= other.number_of_floors
+
     def __gt__(self, other):
         if isinstance(other, House):
             return self.number_of_floors > other.number_of_floors
+
     def __ge__(self, other):
         if isinstance(other, House):
             return self.number_of_floors >= other.number_of_floors
+
     def __ne__(self, other):
         if isinstance(other, House):
             return self.number_of_floors != other.number_of_floors
+
     def __add__(self, value):
         self.number_of_floors = self.number_of_floors + value
         return self.number_of_floors
+
     def __radd__(self, value):
         self.number_of_floors = value + self.number_of_floors
         return self.number_of_floors
+
     def __iadd__(self, value):
         self.number_of_floors += value
         return self.number_of_floors
+    
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 print(h1)
