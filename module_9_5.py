78200d29baf7a19adc430a3adf55a89a3354ec03
petruwka228 <artem.galitskiy.07@mail.ru>
2024-12-19 16:14:20 +0300

    Update module_9_5.py

--- module_9_5.py
+++ module_9_5.py
@@ -1,10 +1,11 @@
 def all_variants(text):
-    length = len(text)
-    for start in range(length):
-        for end in range(start + 1, length + 1):
-            yield text[start:end]
+    for length in range(1, len(text) + 1):
+        for start in range(len(text) - length + 1):
+            yield text[start:start + length]
 
+# Пример использования:
 a = all_variants("abc")
 
 for i in a:
     print(i)
+
