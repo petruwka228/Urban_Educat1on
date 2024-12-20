974de83b3cb29c8c5ff96ef75223d1b12d46ac5e
petruwka228 <artem.galitskiy.07@mail.ru>
2024-12-20 17:00:26 +0300

    Update module_10_1.py

--- module_10_1.py
+++ module_10_1.py
@@ -10,19 +10,22 @@ class Knight(threading.Thread):
         self.monsters = 100
 
     def run(self):
+        lock = threading.Lock()
         print(f"{self.name}, на нас напали!")
         while self.monsters > 0:
+            lock.acquire()
             self.monsters -= self.power
             time.sleep(1)
             self.day += 1
-            print(f"{self.name} сражается {self.day} день(дня)..., осталось {max(self.monsters, 0)} воинов.\n")
-        print(f"{self.name} одержал победу спустя {self.day} дней(дня)!\n")
+            print(f"{self.name} сражается {self.day} день(дня)..., осталось {max(self.monsters, 0)} воинов.")
+            lock.release()
+        lock.acquire()
+        print(f"{self.name} одержал победу спустя {self.day} дней(дня)!")
+        lock.release()
 
 first_knight = Knight('Sir Lancelot', 10)
-
 second_knight = Knight("Sir Galahad", 20)
 first_knight.start()
-time.sleep(1)
 second_knight.start()
 first_knight.join()
 second_knight.join()
