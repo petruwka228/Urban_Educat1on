fbfffbe67ecc91f17b561ef14cadeacd734ca8e3
petruwka228 <artem.galitskiy.07@mail.ru>
2024-12-29 12:53:02 +0300

    m10_4

---
+++ module_10_4.py
@@ -0,0 +1,27 @@
+import time
+from multiprocessing import Pool
+import os
+
+
+def read_info(name):
+    all_data = []
+    with open(name, 'r') as f:
+        while True:
+            s = f.readline()
+            if not s:
+                break
+            all_data.append(s)
+if __name__ == '__main__':
+    t1 = time.time()
+    filenames = [f'file {number}.txt' for number in range(1, 5)]
+    for i in filenames:
+        read_info(i)
+    t2 = time.time()
+    print(t2 - t1, '(линейный)')
+
+    t1 = time.time()
+    with Pool() as pool:
+        pool.map(read_info, filenames)
+    t2 = time.time()
+
+    print(t2 - t1, '(многопроцессный)')
