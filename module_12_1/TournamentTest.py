d707962de5b567b801faab6ae6f674675968e3bc
petruwka228 <artem.galitskiy.07@mail.ru>
2025-01-17 19:26:41 +0300

    module_12_1

---
+++ module_12_1/TournamentTest.py
@@ -0,0 +1,36 @@
+import unittest
+import runner_and_tournament
+
+class TournamentTest(unittest.TestCase):
+    def setUpClass(self):
+        self.all_results = {}
+
+    def setUp(self):
+        self.usain = runner_and_tournament.Runner('Усэйн', 10)
+        self.andrei = runner_and_tournament.Runner('Андрей', 9)
+        self.nik = runner_and_tournament.Runner('Ник', 3)
+
+    def tearDownClass(self):
+        for key, value in self.all_results.items():
+            print(f'{key}: {value}')
+
+    def test_1(self):
+        tournament = runner_and_tournament.Tournament(90, [self.usain, self.nik])
+        result = runner_and_tournament.Tournament.start()
+        self.all_results[0] = result
+        self.assertTrue(result[max(result.keys())] == 'Ник')
+
+    def test_2(self):
+        tournament = runner_and_tournament.Tournament(90, [self.andrei, self.nik])
+        result = runner_and_tournament.Tournament.start()
+        self.all_results[1] = result
+        self.assertTrue(result[max(result.keys())] == 'Ник')
+
+    def test_3(self):
+        tournament = runner_and_tournament.Tournament(90, [self.usain, self.andrei, self.nik])
+        result = runner_and_tournament.Tournament.start()
+        self.all_results[2] = result
+        self.assertTrue(result[max(result.keys())] == 'Ник')
+
+if __name__ == "__main__":
+    unittest.main()
