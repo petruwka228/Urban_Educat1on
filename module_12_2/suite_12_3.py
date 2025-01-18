858bab79b7a798867aac77e182597971e978b20e
petruwka228 <artem.galitskiy.07@mail.ru>
2025-01-18 14:46:03 +0300

    Update suite_12_3.py

--- module_12_2/suite_12_3.py
+++ module_12_2/suite_12_3.py
@@ -7,11 +7,10 @@ suite = unittest.TestSuite()
 suite.addTest(runner_test.RunnerTest('test_challenge'))
 suite.addTest(runner_test.RunnerTest('test_run'))
 suite.addTest(runner_test.RunnerTest('test_walk'))
-suite.addTest(TournamentTest.TournamentTest('test_first_tournament'))
-suite.addTest(TournamentTest.TournamentTest('test_second_tournament'))
-suite.addTest(TournamentTest.TournamentTest('test_third_tournament'))
+suite.addTest(TournamentTest.TournamentTest('test_1'))
+suite.addTest(TournamentTest.TournamentTest('test_2'))
+suite.addTest(TournamentTest.TournamentTest('test_3'))
 
 runner = unittest.TextTestRunner(verbosity=2)
 
 runner.run(suite)
-
